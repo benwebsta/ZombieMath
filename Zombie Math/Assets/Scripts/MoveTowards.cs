505b65083cab8f9aa19bd9dc5bbe55c714890c46
benwebsta <bmwebster@wisc.edu>
2016-03-08 18:54:55 -0600

    first commit, text above zombie head working

---
+++ Zombie Math/Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowards : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update() {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+}
